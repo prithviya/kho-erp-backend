a37bd212c46e156ab0bbd3e53d2fb05ab6ffd474
Nandhakumarm431 <nandhakumar.mmm@firstsource.com>
2026-07-13 18:00:41 +0530

    erp backend setup

---
+++ erp_backend/src/model/index.js
@@ -0,0 +1,35 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+
+    dialect: process.env.DB_TYPE, // postgres or mysql
+
+    logging: false,
+
+    pool: {
+      max: 20,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
+const db = {};
+
+
+// SEQUELIZE
+db.sequelize = sequelize;
+
+db.Sequelize = Sequelize;
+
+
+module.exports = db;
